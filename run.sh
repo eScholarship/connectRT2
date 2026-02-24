647eb329afad3422afc32274792fb8c5f7eab156
Mahjabeen Yucekul <mahjabeen.yucekul@ucop.edu>
2026-02-24 15:30:19 -0800

    Restoring run.sh to pre PumaKiller version

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-exec bundle exec rerun --background --no-notify --restart --signal USR2 "ruby bin/puma -p 5712 -C config/pumakiller.rb -w 2 --tag connectRT2 -e production"
-# puma: bundle exec puma -C config/pumakiller.rb -b unix:///var/run/puma/my_app.sock -t1:16 -w 3
+exec bundle exec rerun --background --no-notify --restart --signal USR2 "ruby bin/puma -p 5712 --tag connectRT2 -e production"
